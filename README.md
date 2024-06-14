1e7b75349d07a11cd5a121341cae5ada2c9fd1d1
algonq <134572776+algonq@users.noreply.github.com>
2024-06-14 17:48:16 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# MTM6302-console-controller
+# MTM6302-console-controller-algonq
+<h1>Name: jatin khanchi<br>student id : 90541117633@algonquincdistudent.ca
+
+![Screenshot 2024-05-31 150856](https://github.com/algonq/MTM6302-console-controller-algonq/assets/134572776/fc03866d-53ee-4990-8ce5-93906c71bb32)
---
+++ Script.js
@@ -0,0 +1,31 @@
+// function to set title
+function setTitle(newtitle)
+{
+    const titleElement = document.grtElementbyId('title');
+}
+//function for description.
+function setDescription(newDescription)
+{
+    const descriptionElement  = document.grtElementbyId('Description');
+    descriptionElenent.textcontent = newDescription;
+}
+// function to set background color.
+function setBackgroundcolor(color)
+{
+    document.body.style.color = color;
+   const allElements = document.querySelectorAll('*')
+}
+// function to set the Theme
+function setTheme(theme)
+{
+    if(theme === 'dark')
+        {
+            Document.body.classlist.add('dark-theme');
+            Document.body.classlist.remove(light-theme)
+        }
+        else if(theme === 'light')
+            {
+                Document.body.classlist.add('light-theme');
+                Document.body.classlist.remove(dark-theme)
+                }
+} 
